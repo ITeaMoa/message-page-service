0a685fead5afbc5b4a2915894ba8588076b50607
Jeonsangeon <wjstkddjs28@gmail.com>
2025-03-29 00:25:18 +0900

    feat.init directory

---
+++ src/main/java/com/iteamoa/message/entity/MessageEntity.java
@@ -0,0 +1,9 @@
+package com.iteamoa.message.entity;
+
+import lombok.Setter;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+
+@Setter
+@DynamoDbBean
+public class MessageEntity {
+}
---
+++ src/main/java/com/iteamoa/message/repository/MessageRepository.java
@@ -0,0 +1,8 @@
+package com.iteamoa.message.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MessageRepository {
+
+}
