d7ee24b2588da58b13c53c4111f1c177d6b503a1
Jeonsangeon <wjstkddjs28@gmail.com>
2025-03-30 14:26:32 +0900

    feat.Exception saveMessage

--- src/main/java/com/iteamoa/message/controller/MessageController.java
+++ src/main/java/com/iteamoa/message/controller/MessageController.java
@@ -3,6 +3,8 @@ package com.iteamoa.message.controller;
 import com.iteamoa.message.dto.MessageDto;
 import com.iteamoa.message.service.MessageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +17,12 @@ public class MessageController {
     private final MessageService messageService;
 
     @PostMapping()
-    public void sendMessage(@RequestBody MessageDto messageDto) {
-        messageService.saveMessage(messageDto);
+    public ResponseEntity<?> sendMessage(@RequestBody MessageDto messageDto) {
+        try{
+            messageService.saveMessage(messageDto);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
--- src/main/java/com/iteamoa/message/service/MessageService.java
+++ src/main/java/com/iteamoa/message/service/MessageService.java
@@ -5,12 +5,18 @@ import com.iteamoa.message.repository.MessageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 @RequiredArgsConstructor
 public class MessageService {
     private final MessageRepository messageRepository;
 
     public void saveMessage(MessageDto messageDto) {
+        Objects.requireNonNull(messageDto.getPk(), "Pk cannot be null");
+        Objects.requireNonNull(messageDto.getSk(), "Sk cannot be null");
+        Objects.requireNonNull(messageDto.getReceiverId(), "ReceiverId cannot be null");
+
         messageRepository.saveMessage(messageDto);
     }
 
